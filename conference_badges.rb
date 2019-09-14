c5ff432b7dfe6f71e6e3336212d24f4d61bda25c
Emil Andreasyan <emil.andreasyan@yahoo.com>
2019-09-14 05:51:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+def badge_maker(name)
+  puts "Hello, my name is #{name}"
+end
+badge_maker("Arel")
+
+
+names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+def batch_badge_creator(name)
+  name.each{|person| puts "Hello, my name is #{person}"}
+end
+batch_badge_creator(names)
+
+def assign_room(name)
+  name.each do |person|
+    puts "Hello, #{perons}, you'll be assigned to room #{each_with_index}"
+end
+assign_room(names)
